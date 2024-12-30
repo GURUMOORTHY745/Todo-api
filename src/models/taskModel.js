5450a3437ba40bb96c938c17cf5e9b6ac1e1d53f
GURUMOORTHY745 <126964208+GURUMOORTHY745@users.noreply.github.com>
2024-12-30 17:41:46 +0530

    Update taskModel.js

--- src/models/taskModel.js
+++ src/models/taskModel.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose');
-// structure of the DB to store
+const { v4: uuidv4 } = require('uuid'); // Import the uuid package
+
+// Define the task schema
 const taskSchema = new mongoose.Schema({
+    taskId: { type: String, unique: true, required: true, default: uuidv4 }, // Generate a unique taskId
     title: { type: String, required: true },
     description: { type: String },
     dueDate: { type: Date },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
+
 // Middleware to update updatedAt field before saving
 taskSchema.pre('save', function (next) {
     this.updatedAt = Date.now();
     next();
 });
+
+// Export the Task model
 module.exports = mongoose.model('Task', taskSchema);
